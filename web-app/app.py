8c3d5c3c1a19a7d17b0cd2c79b1b1919e2bbc762
Royston Pierre-Pinnock <roysto.pierre_pinnoc@iris.co.uk>
2021-04-22 11:57:02 +0100

    changed app function

--- web-app/app.py
+++ web-app/app.py
@@ -1,11 +1,9 @@
 from flask import Flask, url_for
 from flask import render_template
 
-app = Flask(__name__, template_folder='templates')
+app = Flask(__name__)
 
 
 @app.route('/')
 def flask_app():
-    name = "My Single Page Python Application"
-    title = "Python Application"
-    return render_template('test.html', name=name, title=title)
+    return 'My Single Page Python Application'
